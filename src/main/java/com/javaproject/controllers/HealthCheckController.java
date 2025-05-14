4114a0a09c041348ef8b4aaa011db9062ece2f34
Nahidul Islam <nahidkishore99@gmail.com>
2025-05-14 19:01:06 +0600

    Create HealthCheckController.java

---
+++ src/main/java/com/javaproject/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.example.boardgamehub.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
+
